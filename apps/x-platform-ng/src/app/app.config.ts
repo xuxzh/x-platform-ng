31cf79c905d085a543954a4b94368936cef9a365
xu_xz <xxz0528@qq.com>
2025-07-28 13:19:11 +0800

    feat: x-platform-ng添加ng-zorro-antd依赖

--- apps/x-platform-ng/src/app/app.config.ts
+++ apps/x-platform-ng/src/app/app.config.ts
@@ -4,12 +4,22 @@ import {
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { zh_CN, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
 import { appRoutes } from './app.routes';
+
+registerLocaleData(zh);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideHttpClient(),
+    provideAnimations(),
+    provideNzI18n(zh_CN),
   ],
 };
--- apps/x-platform-ng/src/app/app.ts
+++ apps/x-platform-ng/src/app/app.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    NzButtonModule,
+    NzIconModule,
+    NzLayoutModule,
+    NzMenuModule,
+  ],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.less',
